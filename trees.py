8f046785ff4ef824126037e1ed4491357b12d020
Maggie Yan <4everyan@gmail.com>
2021-01-18 02:48:19 -0500

    isValiBST

--- trees.py
+++ trees.py
@@ -14,3 +14,15 @@ def maxDepth(self, root: TreeNode) -> int:
             
         count = 1
         return maxDepthHelper(1, root)
+
+def isValidBST(self, root: TreeNode) -> bool:        
+    def isValidBSTHelper(root, low, high):
+        if not root:
+            return True
+        
+        if root.val >= high or root.val <= low:
+            return False
+        
+        return isValidBSTHelper(root.left, low, root.val) and isValidBSTHelper(root.right, root.val, high)
+    
+    return isValidBSTHelper(root, -math.inf, math.inf)
