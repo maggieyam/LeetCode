2abd268c3fdaca0c317d9c3537988713c89bdfb5
Maggie Yan <4everyan@gmail.com>
2021-01-17 23:10:08 -0500

    reversed list

--- LinkedList.py
+++ LinkedList.py
@@ -30,6 +30,29 @@ def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
     
     return head
 
+def reverseList(self, head: ListNode) -> ListNode:
+        prev = head
+        if not head or not head.next:
+            return head
+        curr = head.next
+        if not curr.next:
+            curr.next = head
+            head.next = None
+            head = curr
+            return head
+        next_node = curr.next
+        curr.next = prev
+        head.next = None
+        
+        while next_node.next:
+            prev = curr
+            curr = next_node
+            next_node = next_node.next
+            curr.next = prev
+        next_node.next = curr
+        head = next_node
+        return head
+
 
 if __name__ == '__main__':
     main()
